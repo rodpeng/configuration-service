8221131d015606aae09583d9813ccaee7fbd68fa
Peng, Rod <Rod.Peng@disney.com>
2018-11-11 13:25:46 +0800

    initial commit

---
+++ src/main/java/com/rodpeng/configuration/service/ConfigServiceApplication.java
@@ -0,0 +1,14 @@
+package com.rodpeng.configuration.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.ConfigServerApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
